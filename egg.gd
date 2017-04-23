c27199ed4cacb88310a39cb3f0c44874b75ca2b4
Elliot Bulmer <sonelliot@gmail.com>
2017-04-23 18:58:04 +0800

    Fix bugs with egg

--- egg.gd
+++ egg.gd
@@ -10,7 +10,16 @@ var critter
 func spawn():
 	var c = critter.instance()
 	get_parent().add_child(c)
-	c.set_global_pos(get_global_pos())
+	
+	var offset = 0
+	if species == 'bugtiger':
+		offset = -20
+	
+	var pos = get_global_pos()
+	pos = pos + Vector2(0, offset)
+	
+	c.set_global_pos(pos)
+
 	queue_free()
 
 func _ready():
